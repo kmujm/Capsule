b046643e45fd8b7b0f83f8c09b66777d15407f0b
Park Jungmin <pmini09@kookmin.ac.kr>
2022-04-13 01:54:56 +0900

    create signup nickname page

--- app/src/main/java/com/example/capsule/NicknameActivity.kt
+++ app/src/main/java/com/example/capsule/NicknameActivity.kt
@@ -1,21 +1,57 @@
 package com.example.capsule
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.isVisible
 
-class NicknameActivity : AppCompatActivity() {
+class NicknameActivity : AppCompatActivity(), TextWatcher {
     private var wordCount: Int = 0
     private val nickName: EditText by lazy {
         findViewById(R.id.nickname)
+    }
+    private val moreThanOneText: TextView by lazy {
+        findViewById(R.id.moreThanOne)
+    }
+    private val submitButton: AppCompatButton by lazy {
+        findViewById(R.id.submitButton)
+    }
+    private val wordCounter: TextView by lazy {
+        findViewById(R.id.wordCounter)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nickname)
-        countWord()
+        nickName.addTextChangedListener(this)
     }
 
-    private fun countWord() {
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        val nicknameFromUser = nickName.text.toString()
+        wordCount = nicknameFromUser.length
+
+        if (!nicknameFromUser.isBlank()) {
+            submitButton.background = getDrawable(R.drawable.activate_button_background)
+            moreThanOneText.isVisible = false
+            moreThanOneText.isEnabled = true
+        } else {
+            submitButton.background = getDrawable(R.drawable.inactivate_button_background)
+            submitButton.isEnabled = false
+            moreThanOneText.isVisible = true
+        }
+        wordCounter.text = "${wordCount}/10"
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+        // TODO("Not yet implemented")
     }
 }
