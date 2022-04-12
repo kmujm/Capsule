a47bd7c58d3ca68c3c4d0e414c710182a793afb0
SangwonYoon <yoonsw0532@naver.com>
2022-04-12 23:06:08 +0900

    서버 연동 필요한 기능 외 구현 완료

--- app/src/main/java/com/example/capsule/EditProfileActivity.kt
+++ app/src/main/java/com/example/capsule/EditProfileActivity.kt
@@ -7,10 +7,7 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 
@@ -69,18 +66,23 @@ class EditProfileActivity : AppCompatActivity() {
                 NicknameDialog(this).setTitle("닉네임 설정").setMessage(editNickname.text.toString() + "은 사용 가능한 닉네임이에요!").setPositiveButton("확인"){
                     //TODO 수정한 닉네임 서버에 저장
                     //TODO 개인정보 화면으로 이동
+                    Toast.makeText(this,"프로필 수정이 완료되었습니다.", Toast.LENGTH_SHORT).show()
                 }.show()
             } else{ // 닉네임이 유효하지 않은 경우
                 val alert = editNickname.context.resources.getDrawable( R.drawable.alert_mark )
                 editNickname.setCompoundDrawablesWithIntrinsicBounds( null, null, alert, null) // alert_mark를 edittext의 오른쪽 부분에 나타나게 함
                 editNickname.background = this.resources.getDrawable(R.drawable.edittext_alert_background)
-                //TODO alert dialog를 띄운다.
+                // alert dialog를 띄운다.
+                val ad = NicknameDialog(this).setTitle("닉네임 설정").setMessage(editNickname.text.toString() + "은 사용 불가능한 닉네임이에요!")
+                ad.setPositiveButton("확인"){
+                    ad.dismiss()
+                }.show()
+                completeButton.background = this.resources.getDrawable(R.drawable.inactivate_button_background)
             }
         }
 
         //TODO 이메일 서버에서 가져와서 보여주기
     }
-
     // nickname의 유효성 검사
     private fun isNicknameValid(nickname : String) : Boolean{
         return nickname.isNotEmpty() && nickname.length <= 10 // 글자 수 제한에 맞는지 확인
@@ -91,7 +93,7 @@ class EditProfileActivity : AppCompatActivity() {
         // TODO 사용중인 닉네임인지 확인
         // TODO 사용중인 닉네임이면 return false
         // TODO 사용중인 닉네임이 아니면 return true
-        return true // TODO 수정 필요
+        return false // TODO 수정 필요
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean { // 현재 포커스된 뷰의 영역이 아닌 다른 곳을 클릭 시 키보드를 내리고 포커스 해제
