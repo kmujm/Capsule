5b3afca4364d689e1120af336aea969708c08c25
SangwonYoon <yoonsw0532@naver.com>
2022-05-30 20:05:11 +0900

    메인 화면 디자인 수정 (1)
    버튼 디자인, 인사글에 들어가 있는 username 강조, 최근 등록 탭 생성

--- app/src/main/java/com/example/capsule/MainActivity.kt
+++ app/src/main/java/com/example/capsule/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.capsule
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.settings)
     }
 
+    val greeting : TextView by lazy{
+        findViewById(R.id.greeting)
+    }
+
+    var username = "USERNAME" // 임시 사용
+
     val objectDetectionButton : Button by lazy{
         findViewById(R.id.objectDetectionButton)
     }
@@ -31,6 +42,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val content = "${username} 님,\n오늘도 캡슐하세요!"
+        val spannableString = SpannableString(content)
+        val start_idx = 0
+        val end_idx = username.length
+
+        spannableString.setSpan(StyleSpan(Typeface.BOLD), start_idx, end_idx, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        greeting.text = spannableString
+
         initObjectDetectionButton()
     }
 
