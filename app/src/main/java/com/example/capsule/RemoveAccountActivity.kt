f5b2a62015b6fea9af604468e0e00d618995baa8
Park Jungmin <pmini09@kookmin.ac.kr>
2022-06-16 14:46:44 +0900

    feat:회원탈퇴페이지 DB 연동

--- app/src/main/java/com/example/capsule/RemoveAccountActivity.kt
+++ app/src/main/java/com/example/capsule/RemoveAccountActivity.kt
@@ -2,10 +2,118 @@ package com.example.capsule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class RemoveAccountActivity : AppCompatActivity() {
+    private val TAG = "RemoveAccountActivity"
+
+    private val backBtn: Button by lazy {
+        findViewById(R.id.btn_RemoveBack)
+    }
+
+    private val removeInfo: TextView by lazy {
+        findViewById(R.id.RemoveInfo)
+    }
+
+    private val userNameView: TextView by lazy {
+        findViewById(R.id.RemoveUserName)
+    }
+
+    private val userEmailView: TextView by lazy {
+        findViewById(R.id.RemoveEmail)
+    }
+
+    private val myCapsuleNum: TextView by lazy {
+        findViewById(R.id.MyCapsuleNum)
+    }
+
+    private val myCapsuleTime: TextView by lazy {
+        findViewById(R.id.CapsuleTimeNum)
+    }
+
+    private val removeAccountBtn: AppCompatButton by lazy {
+        findViewById(R.id.btn_RemoveAccount)
+    }
+
+    private val removeCancelBtn: AppCompatButton by lazy {
+        findViewById(R.id.btn_RemoveCancel)
+    }
+
+    private lateinit var database: DatabaseReference
+    private lateinit var UID: String
+    private lateinit var userNickName: String
+    private lateinit var userEmail: String
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remove_account)
+        database = Firebase.database.reference
+        initMyInfo()
+        initAccountRemoveBtn()
+        initCancelBtn()
+        initBackBtn()
     }
+
+    private fun initBackBtn() {
+        backBtn.setOnClickListener {
+            // 뒤로가기 버튼 클릭시 동작
+        }
+    }
+
+    private fun initCancelBtn() {
+        removeCancelBtn.setOnClickListener {
+            // 취소 버튼 클릭시 동작
+        }
+    }
+
+    private fun initAccountRemoveBtn() {
+        removeAccountBtn.setOnClickListener {
+            // 회원 탈퇴 버튼 클릭시 동작
+        }
+    }
+
+    private fun initMyCapsule() {
+        var cnt = 0
+        UID = "asdfifeiofjn1233"//Firebase.auth.currentUser!!.uid
+        val myRef = database.child("Users").child(UID)
+        // 각 유저 데이터베이스 밑에 존재하는 캡슐 키를 가져오는 코드
+        val capsuleKey = mutableListOf<String>()
+        myRef.get().addOnSuccessListener {
+            it.children.forEach {
+                if (it.key.toString() != "Info") {
+                    cnt += 1
+                }
+            }
+            removeInfo.text = "${userNickName} 님의 \n계정과 캡슐 ${cnt}개가 \n사라집니다."
+            myCapsuleNum.text = cnt.toString() + " 개"
+            myCapsuleTime.text = "NN h"
+        }.addOnFailureListener {
+            //
+        }
+    }
+
+    private fun initMyInfo() {
+        val user = 1//Firebase.auth.currentUser
+        if (user != null) {
+            UID = "asdfifeiofjn1233"//Firebase.auth.currentUser!!.uid
+            database.get().addOnSuccessListener {
+                val userInfo = it.child("Users").child(UID).child("Info").value as HashMap<*, *>
+                userNickName = userInfo.get("nickname").toString()
+                userEmail = userInfo.get("email").toString()
+                userNameView.text = userNickName
+                userEmailView.text = userEmail
+                initMyCapsule()
+            }.addOnFailureListener {
+                Log.d(TAG, "Fail getting user nickname data", it)
+            }
+        }
+    }
+
 }
