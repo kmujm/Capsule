73b99f79fdd8dfbf6b5a2bc319fa300d378f541e
dan-2ee <ekdms7273@naver.com>
2022-06-17 05:30:58 +0900

    잘못된 비밀번호 입력했을시 로직 구현

--- app/src/main/java/com/example/capsule/ResetCheckPWActivity.kt
+++ app/src/main/java/com/example/capsule/ResetCheckPWActivity.kt
@@ -1,15 +1,37 @@
 package com.example.capsule
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableString
 import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.getValue
 
 class ResetCheckPWActivity : AppCompatActivity(), TextWatcher {
+    private val mDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val myRef: DatabaseReference = mDatabase.getReference("Users")
+
+    private var auth = FirebaseAuth.getInstance()
+    private var user = auth.currentUser    // 현재 로그인한 유저
+    private lateinit var userPW:String     // 현재 로그인한 유저의 비밀번호
+
     private val pw: EditText by lazy {
         findViewById(R.id.pw)
     }
@@ -20,6 +42,9 @@ class ResetCheckPWActivity : AppCompatActivity(), TextWatcher {
         findViewById(R.id.btn_Back)
     }
 
+    private var clickable: Boolean = false
+    private var clear: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_check_pw)
@@ -27,6 +52,7 @@ class ResetCheckPWActivity : AppCompatActivity(), TextWatcher {
         pw.addTextChangedListener(this)
 
         initBackButton()
+        initCompleteButton()
     }
 
     override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -38,8 +64,10 @@ class ResetCheckPWActivity : AppCompatActivity(), TextWatcher {
 
         if (currentPW.length >= 8) {   // 입력한 비밀번호가 8자 이상이면 완료 버튼 활성화
             btnComplete.background = getDrawable(R.drawable.activate_button_background)
+            clickable = true
         } else {
             btnComplete.background = getDrawable(R.drawable.inactivate_button_background)
+            clickable = false
         }
     }
 
@@ -50,5 +78,71 @@ class ResetCheckPWActivity : AppCompatActivity(), TextWatcher {
         btnBack.setOnClickListener {
             finish()
         }
+    }
+
+    private fun initCompleteButton() {
+        btnComplete.setOnClickListener {
+            if (clickable) {
+                checkPW()
+            }
+        }
+    }
+
+    private fun checkPW() {
+        // var uid = user!!.uid
+        // Todo: uid로 변경
+        // 로그인한 유저의 password 가져옴
+        myRef.child("asdfifeiofjn1233").child("Info").get().addOnSuccessListener {
+            userPW = it.child("password").getValue<String>().toString()
+
+            var inputPW = pw.text.toString()
+            if (inputPW != userPW) {   // 잘못된 비밀번호를 입력한 경우
+                initDialog()
+                clear = false
+                // 버튼 비활성화 + 라인 색 변경
+                btnComplete.background = getDrawable(R.drawable.inactivate_button_background)
+                clickable = false
+                pw.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.cost)
+
+                // 입력창 터치하면 다 지워지고 처음상태로 변경
+                resetPw()
+            }
+        }
+    }
+
+    private fun resetPw() {
+        pw.setOnClickListener {
+            if (!clear) {
+                pw.setText("")
+                pw.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.catagory_icon_brown)
+                clear = true
+            }
+        }
+    }
+
+    private fun initDialog() {
+        val dlg = Dialog(this)
+        dlg.setContentView(R.layout.pw_check_dialog)
+        dlg.setCancelable(false)
+        dlg.window?.setBackgroundDrawable(Drawable.createFromPath("@drawable/alert_dialog_background"))
+
+        // dialog message
+        var tvMessage : TextView = dlg.findViewById(R.id.content)
+        var message = tvMessage.text.toString()
+        val spannableString = SpannableString(message)
+
+        // 비밀번호 문자열만 bold 변경
+        var word = "비밀번호"
+        val start = message.indexOf(word)
+        val end = start + word.length
+
+        spannableString.setSpan(StyleSpan(Typeface.BOLD), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tvMessage.text = spannableString
+
+        var btnOK : Button = dlg.findViewById(R.id.btn_ok)
+        btnOK.setOnClickListener {
+            dlg.dismiss()
+        }
+        dlg.show()
     }
 }
