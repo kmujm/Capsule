a5b03575802ad8c2bbcc403d3f90c6c15856ce7d
SangwonYoon <yoonsw0532@naver.com>
2022-06-18 00:41:37 +0900

    EditProfileActivity firebase auth 연결 및 로직 수정
    firestore -> realtime DB

--- app/src/main/java/com/example/capsule/EditProfileActivity.kt
+++ app/src/main/java/com/example/capsule/EditProfileActivity.kt
@@ -13,13 +13,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class EditProfileActivity : AppCompatActivity() {
 
-    private val db : FirebaseFirestore = Firebase.firestore
+    private val database  = Firebase.database.reference
     private lateinit var auth: FirebaseAuth
 
     private val backButton : ImageButton by lazy{
@@ -43,21 +44,17 @@ class EditProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_editprofile)
 
         auth = Firebase.auth
-        val uid = auth.currentUser?.uid ?: "test" // TODO 임시 테스트용
+        val uid = auth.currentUser!!.uid
 
         // 유저의 정보 가져오기
-        db.document("users/${uid}").get().addOnCompleteListener{ task ->
-            if(task.isSuccessful){
-                val document = task.result
-                val email = document.data?.get("Email")
-                val nickname = document.data?.get("Nickname")
+        database.child("Users").child(uid).child("Info").get().addOnSuccessListener{
+            val email = it.child("email").value.toString()
+            val nickname = it.child("nickname").value.toString()
 
-                //이메일 서버에서 가져와서 보여주기
-                editEmail.setText(email.toString())
-                //닉네임 서버에서 가져와서 보여주기
-                editNickname.setText(nickname.toString())
-            }
-
+            //이메일 서버에서 가져와서 보여주기
+            editEmail.setText(email.toString())
+            //닉네임 서버에서 가져와서 보여주기
+            editNickname.setText(nickname.toString())
         }
 
         backButton.setOnClickListener{
@@ -99,9 +96,9 @@ class EditProfileActivity : AppCompatActivity() {
     private fun checkNickname(nickname: String, uid : String){
         if(isNicknameValid(nickname)){
             var flag = true
-            db.collection("users").get().addOnSuccessListener {
-                for (document in it) {
-                    val temp = document.data?.get("Nickname").toString()
+            database.child("Users").get().addOnSuccessListener {
+                it.children.forEach {p0 ->
+                    val temp = p0.child("Info").child("nickname").value.toString()
                     Log.d("log", temp)
                     Log.d("log", (temp == nickname).toString())
                     if (temp == nickname) {
@@ -133,17 +130,13 @@ class EditProfileActivity : AppCompatActivity() {
                         .setMessage(editNickname.text.toString() + "은 사용 가능한 닉네임이에요!")
                     ad.setPositiveButton("확인") {
                         // 수정한 닉네임 서버에 저장
-                        val newNickname = mutableMapOf<String, Any>()
-                        newNickname["Nickname"] = editNickname.text.toString()
-                        db.document("users/${uid}").update(newNickname).addOnCompleteListener{ task ->
-                            if(task.isSuccessful){
-                                ad.dismiss()
-                                //개인정보 화면으로 이동
-                                finish()
-                                //TODO 개인정보 Activity의 onResume()에서 액티비티 새로고침 내용 추가
-                                Toast.makeText(this, "프로필 수정이 완료되었습니다.", Toast.LENGTH_SHORT).show()
-                            }
-                        }
+                        val newNickname = editNickname.text.toString()
+                        database.child("Users").child(uid).child("Info").child("nickname").setValue(newNickname)
+                        ad.dismiss()
+                        //개인정보 화면으로 이동
+                        finish()
+                        //TODO 개인정보 Activity의 onResume()에서 액티비티 새로고침 내용 추가
+                        Toast.makeText(this, "프로필 수정이 완료되었습니다.", Toast.LENGTH_SHORT).show()
                     }.show()
                 }
             }
